dc5acb6fe90463603c6808f9b395d0faf1c61d28
Derek <legato_5@yahoo.com>
2019-10-13 19:44:52 -0700

    added styling for pages

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,14 +1,22 @@
 import React from "react";
 import CharacterList from './CharacterList.js';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
 export default function CharacterCard(props) {
   return (
     <div className="char-card">
-        <img src={props.image} />
-        <h2>{props.name}</h2>
-        <p>{props.species}</p>
-        <p>{props.origin}</p>
-        <p>{props.stat}</p>
+      <Card style={{marginBottom: '5%'}}>
+        <CardImg top width="100%"  src={props.image} alt="Character image" />
+        <CardBody style={{backgroundColor: 'green'}}>
+          <CardTitle style={{color: 'white'}}>{props.name}</CardTitle>
+          <CardSubtitle style={{color: 'white'}}>{props.species}</CardSubtitle>
+          <CardText style={{color: 'white'}}>{props.stat}</CardText>
+        </CardBody>
+      </Card>
+        
     </div>
   )
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { CardImg } from 'reactstrap';
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <Link to = {"/characters/"}><img
-          className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
-        /></Link>
+        <Link to = {"/characters/"}>
+        <CardImg top width="100%" src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="rick" style={{height: '40rem'}}/></Link>
       </header>
     </section>
   );
