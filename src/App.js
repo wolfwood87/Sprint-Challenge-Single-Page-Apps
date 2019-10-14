02fce980802a40fa971ee0729e34b41784e4f90b
Derek <legato_5@yahoo.com>
2019-10-14 14:26:39 -0700

    added link to homepage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Header from "./components/Header.js";
 import CharacterList from './components/CharacterList.js';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import WelcomePage from './components/WelcomePage.js';
 import SearchForm from './components/SearchForm';
 
 export default function App() {
   return (
     <main>
-      <Header />
       <Route path = "/" component={SearchForm} />
+      <Link to = {"/"}><Header /></Link>
       <Route exact path="/" component={WelcomePage} />
       <Route path = "/characters" component={CharacterList} />
     </main>
